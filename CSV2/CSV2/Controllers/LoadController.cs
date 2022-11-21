fa053125a72b95b353a01ff8d8e8992d3501389c
Sucheeej <Suchej519@seznam.cz>
2022-11-21 10:36:54 +0100

    TeamMartin & Mikolášek
    Celý jsem to udělal 'char'

---
+++ CSV2/CSV2/Controllers/LoadController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace CSV2.Controllers
+{
+    public class LoadController : Controller
+    {
+        private string _fileName;
+        public IActionResult Index()
+        {            
+            string[]data =_fileName.Split(';');
+            return Content(_fileName);
+        }
+        public LoadController()
+        {
+            _fileName = 
+            System.IO.File.ReadAllText("input.csv");
+            
+        }
+
+    }
+}
