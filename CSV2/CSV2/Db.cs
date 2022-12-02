09f4535285badc55b991d8a7b75477f09a2a0762
doggycs <46246142+doggycs@users.noreply.github.com>
2022-12-02 12:06:04 +0100

    RadimDB
    Lalala

---
+++ CSV2/CSV2/Db.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
+
+namespace CSV2
+{
+    public class Db:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("localhost");
+        }
+    }
+}
