5656fcc40243bd0bf618f00e23500324fb02eb35
radimkohout <radimm.kohout@gmail.com>
2022-12-02 17:24:48 +0100

    Updated the structure

---
+++ BuilderServer/Controllers/LoadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace CSV2.Controllers
+{
+    public class LoadController : Controller
+    {
+        private string _fileName;
+        public IActionResult Index()
+        {            
+            string[]data =_fileName.Split(';');
+            return Content(_fileName);
+        }
+        public LoadController()
+        {
+            _fileName = 
+            System.IO.File.ReadAllText("input.csv");
+        }
+        public string Get(int? maxTime, string? Name)
+        {
+            return $"{maxTime}:{Name}";
+        }
+
+    }
+}
